ce11f2e06439ea373bab9aca3106378f99052086
41110wd <philip.bramwell@gmx.de>
2018-03-20 12:09:03 +0100

    Application is able to convert from binary to decimal

---
+++ C#/Bin_Hex_Dec_Converter_Class_Lib/Bin_Hex_Dec_Converter_Class_Lib/Converting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bin_Hex_Dec_Converter_Class_Lib
+{
+    public class Converting
+    {
+
+        public int GetDec(int bin)
+        {
+            int buffer;
+            int result = 0;
+            for(int i = 0;bin>0;i++)
+            {
+                buffer = bin % 10;
+                if(buffer>1)
+                {
+                    return -1;
+                }
+                result += (int)(buffer * Math.Pow(2,i));
+                bin /= 10;
+            }
+            return result;
+        }
+    }
+}
---
+++ C#/Bin_Hex_Dec_Converter_Class_Lib/Bin_Hex_Dec_Converter_Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bin_Hex_Dec_Converter_Class_Lib;
+
+namespace Bin_Hex_Dec_Converter_Console
+{
+    class Program
+    {
+        static Converting conv;
+
+        static void Main(string[] args)
+        {
+            conv = new Converting();
+            int result;
+            int bin;
+
+            Console.Write("Please enter binary Number: ");
+
+            try
+            {
+                bin = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                result = conv.GetDec(bin);
+                if(result == -1)
+                {
+                    Console.WriteLine("No binary Number was entered");
+                }
+                else
+                    Console.WriteLine($"Binary\t\tDecimal\n--------------------------------\n{bin}\t\t{result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
